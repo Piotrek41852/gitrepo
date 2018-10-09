1544a2e35dd139ba6cc2017772ecb55226f9ceef
Piotrek41852 <Piotrek41852@Gmail.com>
2018-10-09 12:35:35 +0200

    figury123

--- python/figury.py
+++ python/figury.py
@@ -4,26 +4,46 @@
 #  figury.py
 #  
 #
-#  
-
-
-def main(args):
-    a = int(input("Podaj długość boku"))
-    b = int(input("Podaj długość drugiego boku"))
-    znak = input("Podaj znak")
-    
+def prostokąt1(a, b, znak):
     for i in range(a):
         for j in range(b):
             print(znak , end='' )
         print()
         
+        
+def prostokąt2(a, b, znak):
     for i in range(a):
         for j in range(b):
-            if j == 0 or j ==b -1:
-            print(znak , end='' )
-            print()
-        else:
-            print(" ", end,'' )
+            if j == 0 or j ==b -1 or i == 0 or i == a - 1:
+                print(znak, end='')
+            else:
+                print(" ", end='')
+        print()
+        
+        
+def choinka1(h, znak):
+    for i in range(h):
+        if j == 0 or i == h + 1:
+            print(znak, end='')
+        print()
+    
+    
+    
+
+
+
+def main(args):
+    a = int(input("Podaj długość boku: "))
+    b = int(input("Podaj długość drugiego boku: "))
+    h = int(input("Podaj wysokość ")
+    znak = input("Podaj znak: ")
+    prostokąt1(a, b, znak)
+    print()
+    prostokąt2(a, b, znak)
+    print()
+    choinka1(h, znak)
+    
+       
         
     return 0
 
