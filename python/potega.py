8a234652f48b6874e5d2fb4a940b73b2aff2599b
Piotrek41852 <Piotrek41852@Gmail.com>
2018-10-16 12:36:45 +0200

    silnia

--- python/potega.py
+++ python/potega.py
@@ -10,12 +10,16 @@ def potega_it(a, n):
     
     for i in range(n):
         wynik = wynik * a
-        print(wynik)
+        #print(wynik)
     return wynik
 
 def main(args):
-    a, n = 3, 4
-    print("Potega {} do {}".format(a, n, potega_it(a, n)))
+    #a, n = 3, 4
+    #print("Potega {} do {}".format(a, n, potega_it(a, n)))
+    assert(potega_it(0, 2) == 0)
+    assert(potega_it(1, 2) == 1)
+    assert(potega_it(2, 2) == 4)
+    
     return 0
 
 if __name__ == '__main__':
---
+++ python/silnia.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  potega.py
+#  
+
+def silnia_it(n):
+    # 0! = 1
+    # n! = 1 *...* dla <1;n>
+    
+
+    
+    wynik = 1
+    
+    
+    for i in range(1, n + 1):
+        wynik = wynik * i
+        #print(wynik)
+        
+    return wynik
+
+def main(args):
+    #n = 4
+    #rint("{}! = {}".format(n, silnia_it(n)))
+    assert(silnia_it(0) == 1)
+    assert(silnia_it(4) == 24)
+    assert(silnia_it(10) == 3628800)
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
