4891de88ef61f6c7a0217e1f51577416c2dc2732
Piotrek41852 <Piotrek41852@Gmail.com>
2018-10-24 08:45:10 +0200

    euklidescpp

---
+++ cpp/euklides/main.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+
+using namespace std;
+
+int nwd_klasyczny(int a, int b) {
+    while (a != b) {
+        if(a> b)
+            a = a -b;
+        else
+            b = b -a;
+    }
+    return a;
+}
+
+
+int main()
+{
+    int a, b;
+    a = b = 0;
+    cout << "Podaj 2 liczby: ";
+    cin >> a >> b;
+    cout << "NWD(" << a << ", " << b << ") = " << nwd_klasyczny(a, b) << endl;
+
+
+    return 0;
+}
