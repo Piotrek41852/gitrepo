c9aab26a97d9d71ae85248f97e12b60631c69313
Piotrek41852 <Piotrek41852@Gmail.com>
2018-10-25 07:57:06 +0200

    euklides2 cpp

--- cpp/euklides/main.cpp
+++ cpp/euklides/main.cpp
@@ -4,15 +4,37 @@
 using namespace std;
 
 int nwd_klasyczny(int a, int b) {
+    int licznik = 0;
     while (a != b) {
         if(a> b)
             a = a -b;
         else
             b = b -a;
+        licznik++;
     }
+    cout << "Powtórzeń: " << licznik << endl;
     return a;
 }
 
+
+int nwd_optymalny(int a, int b) {
+    int licznik = 0;
+    while (a > 0) {
+        if (a > 0){
+            a = a % b;
+            b = b - a;
+            licznik++;
+        }else
+            cout <<  b;
+    }
+
+
+
+    cout << "Powtórzeń: " << licznik << endl;
+    return b;
+
+
+}
 
 int main()
 {
@@ -20,8 +42,10 @@ int main()
     a = b = 0;
     cout << "Podaj 2 liczby: ";
     cin >> a >> b;
+    int wynik = nwd_klasyczny(a, b);
     cout << "NWD(" << a << ", " << b << ") = " << nwd_klasyczny(a, b) << endl;
-
+    wynik = nwd_optymalny(a, b)
+    cout << "NWD(" << a << ", " << b << ") = " << nwd_optymalny(a, b) << endl;
 
     return 0;
 }
