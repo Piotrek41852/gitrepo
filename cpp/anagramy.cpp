764ba7d8d4981c0794da0224c9dcf8aa425ef6b3
Piotrek41852 <Piotrek41852@Gmail.com>
2018-11-14 08:46:14 +0100

    palindromy

--- cpp/anagramy.cpp
+++ cpp/anagramy.cpp
@@ -14,8 +14,11 @@ void anagramy(char w[], int r) {
         for (i1 = 0; i1 < r ; i1++) {
             for (i2 = 0; i2 < r; i2++) {
                 if (i1 == i2) continue;
-                cout << i1 << i2 << endl;
-                    
+                for (i3 = 0; i3 < r; i3++){
+                    if (i1 == i3 || i2 == i3) continue;
+                    i4 = 6 - (i1 + i2 + i3);
+                    cout << w[i1] << w[i2] << w[i3] << w[i4] << endl;  
+            }                    
         }
     }
 }
@@ -27,6 +30,7 @@ int main(int argc, char **argv)
 	const int rozmiar = 4;
     char wyraz[] = "ryba"; 
     anagramy(wyraz, rozmiar);
+
     
     
     
